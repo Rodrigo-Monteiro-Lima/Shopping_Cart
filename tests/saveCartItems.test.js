fa7d212f1d55a4e5d0dfd0768f2f85c2680808c0
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-09-12 17:32:59 -0300

    sexto requisito

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,16 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se o método localStorage.setItem é chamado', () => {
+    expect.assertions(1);
+    const li = '<li>Item</li>';
+    saveCartItems(li)
+    expect(localStorage.setItem).toHaveBeenCalled()
+  });
+  it('Teste se  o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave cartItems e o segundo sendo o valor passado como argumento para saveCartItems', () => {
+    expect.assertions(1);
+    const li = '<li>Item</li>';
+    saveCartItems(li)
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', li)
+  });
 });
